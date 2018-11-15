d14852ce78a48f2c97869f5d82b2b0806dd2d853
LoRd_MuldeR <mulder2@gmx.de>
2018-11-15 23:21:27 +0100

    Initial commit.

---
+++ src/msleep.c
@@ -0,0 +1,132 @@
+/*
+ * msleep for Win32
+ * Created by LoRd_MuldeR <mulder2@gmx.de>.
+ * 
+ * This work is licensed under the CC0 1.0 Universal License.
+ * To view a copy of the license, visit:
+ * https://creativecommons.org/publicdomain/zero/1.0/legalcode
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <errno.h>
+
+#define WIN32_LEAN_AND_MEAN 1
+#include <Windows.h>
+
+//VC 6.0 workaround
+_CRTIMP extern FILE _iob[];
+#undef stderr
+#define stderr (&_iob[2])
+
+/* ======================================================================= */
+/* UTILITY FUNCTIONS                                                       */
+/* ======================================================================= */
+
+static __forceinline unsigned long parseULong(const wchar_t *str, unsigned long *const out)
+{
+	unsigned long long value;
+	char c;
+	while(isspace(*str))
+	{
+		str++;
+	}
+	if(swscanf(str, _wcsnicmp(str, L"0x", 2) ? L"%llu %c" : L"%llx %c", &value, &c) != 1)
+	{
+		return EINVAL;
+	}
+	if(value > ULONG_MAX)
+	{
+		return ERANGE;
+	}
+	*out = (unsigned long) value;
+	return 0;;
+}
+
+static __forceinline unsigned long long fileTimeToMSec(const FILETIME *const fileTime)
+{
+	ULARGE_INTEGER tmp;
+	tmp.HighPart = fileTime->dwHighDateTime;
+	tmp.LowPart = fileTime->dwLowDateTime;
+	return tmp.QuadPart;
+}
+
+static __forceinline unsigned long long getStartupTime(void)
+{
+	FILETIME timeCreation, timeExit, timeKernel, timeUser;
+	GetProcessTimes(GetCurrentProcess(), &timeCreation, &timeExit, &timeKernel, &timeUser);
+	return fileTimeToMSec(&timeCreation);
+}
+
+static __forceinline unsigned long computeDelta(const unsigned long long begin, const unsigned long long end)
+{
+	if(end > begin)
+	{
+		const unsigned long long delta = (end - begin) / 10000ULL;
+		return (delta < ULONG_MAX) ? ((unsigned long)delta) : ULONG_MAX;
+	}
+	return 0UL;
+}
+
+static __forceinline unsigned long long getCurrentTime(void)
+{
+	FILETIME now;
+	GetSystemTimeAsFileTime(&now);
+	return fileTimeToMSec(&now);
+}
+
+static BOOL __stdcall crtlHandler(DWORD dwCtrlTyp)
+{
+	switch (dwCtrlTyp)
+	{
+	case CTRL_C_EVENT:
+		fputs("Ctrl+C: Sleep has been interrupted !!!\n\n", stderr);
+		break;
+	case CTRL_BREAK_EVENT:
+		fputs("Break: Sleep has been interrupted !!!\n\n", stderr);
+		break;
+	default:
+		return FALSE;
+	}
+	fflush(stderr);
+	_exit(2);
+	return TRUE;
+}
+
+/* ======================================================================= */
+/* MAIN                                                                    */
+/* ======================================================================= */
+
+int wmain(int argc, wchar_t *argv[])
+{
+	unsigned long timeout, delta;
+
+	SetErrorMode(SetErrorMode(0x0003) | 0x0003);
+	setlocale(LC_ALL, "C");
+	SetConsoleCtrlHandler(crtlHandler, 1);
+
+	if (argc < 2)
+	{
+		fputs("msleep [" __DATE__ "]\n\nUsage:\n   msleep.exe <timeout_ms>\n\n", stderr);
+		return 1;
+	}
+
+	switch (parseULong(argv[1], &timeout))
+	{
+	case EINVAL:
+		fputs("Error: Given timeout value could not be parsed!\n\n", stderr);
+		return 1;
+	case ERANGE:
+		fputs("Error: Given timeout value is out of range!\n\n", stderr);
+		return 1;
+	}
+
+	delta = computeDelta(getStartupTime(), getCurrentTime());
+	if (timeout >= delta)
+	{
+		Sleep(timeout - delta);
+	}
+
+	return 0;
+}
