979e70f224a9da30a5eeff1b5e8199de8ad0ca34
LoRd_MuldeR <mulder2@gmx.de>
2018-11-24 21:00:35 +0100

    Added version info.

---
+++ src/version.h
@@ -0,0 +1,28 @@
+/*
+ * FS utility functions
+ * Created by LoRd_MuldeR <mulder2@gmx.de>.
+ * 
+ * This work is licensed under the CC0 1.0 Universal License.
+ * To view a copy of the license, visit:
+ * https://creativecommons.org/publicdomain/zero/1.0/legalcode
+ */
+
+#pragma once
+
+///////////////////////////////////////////////////////////////////////////////
+// LameXP Version Info
+///////////////////////////////////////////////////////////////////////////////
+
+#define VER_MSLEEP_MAJOR      1
+#define VER_MSLEEP_MINOR_HI   0
+#define VER_MSLEEP_MINOR_LO   2
+#define VER_MSLEEP_PATCH      0
+
+///////////////////////////////////////////////////////////////////////////////
+// Helper macros (aka: having fun with the C pre-processor)
+///////////////////////////////////////////////////////////////////////////////
+
+#define ___VER_MSLEEP_STR___(X)       #X
+#define __VER_MSLEEP_STR__(W,X,Y,Z)   ___VER_MSLEEP_STR___(v##W.X##Y-Z)
+#define _VER_MSLEEP_STR_(W,X,Y,Z)     __VER_MSLEEP_STR__(W,X,Y,Z)
+#define VER_MSLEEP_STR                _VER_MSLEEP_STR_(VER_MSLEEP_MAJOR,VER_MSLEEP_MINOR_HI,VER_MSLEEP_MINOR_LO,VER_MSLEEP_PATCH)
